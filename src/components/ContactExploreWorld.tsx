3d65174cd64636a47068ee136607355db7c03187
Younis1996-code <younisissa205@gmail.com>
2025-08-06 17:23:56 +0300

    modified-src/components/ContactExploreWorld.tsx&src/sections/Contact/ContactExploreWorldContent.tsx

--- src/components/ContactExploreWorld.tsx
+++ src/components/ContactExploreWorld.tsx
@@ -4,7 +4,7 @@ import ContactExploreWorldImages from "../sections/Contact/ContactExploreWorldIm
 
 const ContactExploreWorld = () => {
   return (
-    <div className="mx-4 lg:mx-[5.5556vw] 2xl:mx-[8.4375vw] bg-[url('/assets/images/Contact/ExploreBackground.webp')] p-[6.1538vw] lg:p-[4.1667vw] 2xl:p-[5.5556vw] bg-darkGray border border-borderColor rounded-xl overflow-hidden flex flex-col gap-[14.35897vw] md:gap-5">
+    <div className="mx-4 lg:mx-[5.5556vw] 2xl:mx-[8.4375vw] bg-[url('/assets/images/Contact/ExploreBackground.webp')] p-[6.1538vw] lg:p-[4.1667vw] 2xl:p-[5.5556vw] bg-darkGray border border-borderColor rounded-xl overflow-hidden flex flex-col gap-[14.35897vw] lg:gap-5">
       <ContactExploreWorldImages photos={contactExplorePhotosData.photos} />
 
       <ContactExploreWorldContent
--- src/sections/Contact/ContactExploreWorldContent.tsx
+++ src/sections/Contact/ContactExploreWorldContent.tsx
@@ -1,5 +1,6 @@
 import CardImage from "../../components/CardImage";
-import IconTitleDescription from "../../components/sharedComponents/IconTitleDescription";
+import SectionHeader from "../../components/SectionHeader";
+
 import type { ContactHeroDataType } from "../../types/Contact/ContactType";
 
 const ContactExploreWorldContent = ({
@@ -11,13 +12,20 @@ const ContactExploreWorldContent = ({
 }) => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-      <IconTitleDescription
+
+      <div className="my-auto">
+        <SectionHeader title={mainText.title}
+        description={mainText.description}
+        withIcon />
+      </div>
+
+      {/* <IconTitleDescription
         title={mainText.title}
         description={mainText.description}
         showIcon={true}
         icon="/assets/icons/stars-of-title.svg"
         className="my-auto"
-      />
+      /> */}
 
       <CardImage
         src={mainImage}
