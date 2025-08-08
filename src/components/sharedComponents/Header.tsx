abde2681b94998056bc3249b57eafe1535ece1d1
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-08-08 22:58:35 +0300

    Edit-Feature|delete Header of section and edit Heder|edit some breakpoint and more props

--- src/components/sharedComponents/Header.tsx
+++ src/components/sharedComponents/Header.tsx
@@ -6,16 +6,18 @@ const Header = ({
   description,
   withIcon,
   withBtn,
-  gap = "gap-2"
+  gap = "gap-2",
+  className = ""
 }: {
   title?:string,
   description?:string,
   withIcon?:boolean,
   withBtn?:boolean,
-  gap ?: string
+  gap ?: string,
+  className ?: string
 }) => {
   return (
-    <div className={`relative flex flex-col ${gap} md:gap-2.5 lg:gap-3.5`}>
+    <div className={`relative flex flex-col ${gap} md:gap-2.5 lg:gap-3.5 ${className}`}>
       {/* stars icon*/}
       {withIcon && (
         <div className="absolute top-[-20px] md:top-[-30px] left-[-8px] md:left-[-16px]">
@@ -30,7 +32,7 @@ const Header = ({
 
       {/* description */}
       <div className="flex items-center justify-between gap-37.5 2xl:gap-50">
-        <p className={`text-Grey-60 font-medium text-sm md:text-base xl:text-[18px]`}>
+        <p className={`text-Grey-60 font-medium text-sm md:text-base xl:text-lg leading-[150%]`}>
         {description}
       </p>
       {withBtn && <Button content="view all property" className="bg-Grey-10 hidden md:block" withBorder />}
