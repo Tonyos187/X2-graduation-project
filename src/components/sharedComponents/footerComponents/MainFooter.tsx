9ec159a41549a37b872bf6627c6595f3b5050f77
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-08-09 21:42:33 +0300

    Add-Feature|يثؤ part of Footer

--- src/components/sharedComponents/footerComponents/MainFooter.tsx
+++ src/components/sharedComponents/footerComponents/MainFooter.tsx
@@ -1,8 +1,13 @@
+import Logo from "../Logo"
 
 function MainFooter() {
   return (
-    <div>
-      
+    <div className="flex flex-col px-4 py-12.5 gap-12.5">
+      <div className="flex flex-col gap-5">
+        <Logo/>
+        
+      </div>
+      <div></div>
     </div>
   )
 }
