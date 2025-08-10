3eca06626bac4b92fa590061c1bb9c4801135a39
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-08-10 21:27:31 +0300

    Edit-Feature|main footer

--- src/sections/sharedSections/Footer.tsx
+++ src/sections/sharedSections/Footer.tsx
@@ -1,13 +1,13 @@
 import EndFooter from "../../components/sharedComponents/footerComponents/EndFooter";
 import MainFooter from "../../components/sharedComponents/footerComponents/MainFooter";
 import TopFooter from "../../components/sharedComponents/footerComponents/TopFooter";
-import { ctaSectionData, endFooterData, navigationColumn } from "../../data/Home/HomeData";
+import { ctaSectionData, endFooterData, footerLayout } from "../../data/Home/HomeData";
 
 const Footer = () => {
   return (
     <footer className="mt-20 md:mt-30 xl:mt-37.5">
       <TopFooter data={ctaSectionData}/>
-      <MainFooter navigationColumn={navigationColumn}/>
+      {/* <MainFooter  navigationColumn = {footerLayout}/> */}
       <EndFooter endFooterData={endFooterData}/>
     </footer>
   );
