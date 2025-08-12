858e938abaf812328ecb07f8f61465324cc4eb8d
YazanTawila <infoyazantawila@gmail.com>
2025-08-13 00:24:14 +0300

    Edit | src/components/cads/PropertiesCard -edit images height-

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,9 +1,23 @@
-import PropertyDetailsSlider from "../sections/propertiesDetails/PropertyDetailsSlider";
+import PropertiesCard from "../components/cards/PropertiesCard";
+import { featuredPropertiesSectionData } from "../data/Home/HomeData";
+import ComprehensiveDetails from "../sections/propertiesDetails/ComprehensiveDetails";
+
+
 
 const PropertyDetails = () => {
+  const data =featuredPropertiesSectionData.properties
   return (
-    <div className="space-y-20 md:space-y-30 xl:space-y-37.5">
-      <PropertyDetailsSlider />
+    <div className="space-y-20 md:space-y-30 xl:space-y-37.5 ">
+      <ComprehensiveDetails/>
+      
+            {/* <div className="flex justify-between flex-wrap">
+              {data.map((dat,index) => {
+                return(
+                    <PropertiesCard showInfo={true} key={dat.id} details={dat.details} image={dat.image} price={dat.price} title={dat.title} lessDescription={dat.description}/>
+                )
+            })} 
+            </div> */}
+      
     </div>
   );
 };
