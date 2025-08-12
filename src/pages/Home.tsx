35203e9575e04c01eb207cb1f5bc65ac0b61f320
unknown <wajdtitos@gmail.com>
2025-08-12 20:23:46 +0300

    fix error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
+import PropertyInfoSection from "../sections/FeatureAndDesc/PropertyInfoSection";
 import Hero from "../sections/HomePageSections/Hero";
-
 
 
 const Home = () => {
   return (
     <>
     <Hero/>
+    <PropertyInfoSection />
     </>
   );
 };
--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,5 +1,4 @@
 import PropertyDetailsSlider from "../sections/propertiesDetails/PropertyDetailsSlider";
-
 const PropertyDetails = () => {
   return (
     <div className="space-y-20 md:space-y-30 xl:space-y-37.5">
