2f62ffee383d030681cefb9f7adf8c396bf5ab59
unknown <wajdtitos@gmail.com>
2025-08-13 14:03:20 +0300

    Add PropertyInfoSection section in its page and fix padding & gap in containers

--- src/sections/FeatureAndDesc/FeaturesSection.tsx
+++ src/sections/FeatureAndDesc/FeaturesSection.tsx
@@ -5,7 +5,7 @@ type FeaturesSectionProps = { title: string; features: FeaturesType[] };
 
 export default function FeaturesSection({ title, features }: FeaturesSectionProps) {
   return (
-    <div className="min-h-[514px] rounded-[12px] border border-Grey-15 p-[40px]  mt-5 lg:mt-0 flex flex-col gap-[40px] bg-Grey-08">
+    <div className="min-h-[514px] rounded-[12px] border border-Grey-15 p-5 lg:p-10 xl:p-[50px]  mt-5 lg:mt-0 flex flex-col gap-[40px] bg-Grey-08">
       <h2 className="text-White  text-lg lg:text-[20px] xl:text-2xl font-semibold">{title}</h2>
       <div className="flex flex-col gap-[18px] md:gap-5 xl:gap-[30px]">
         {features.map((feature, index) => (
