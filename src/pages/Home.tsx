8c1196dec4bcd0c07048dedd504a7533db916789
asmaaAlmohammed-webDev <asmaa811almohammed@gmail.com>
2025-08-13 22:56:54 +0300

    comm

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import HowItWorksCard from "../components/cards/HowItWorksCard";
 import Hero from "../sections/HomePageSections/Hero";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <HowItWorksCard
+        step="01"
+        title="Narrowing Down Your Choices"
+        description="Once you've found properties that catch your eye, save them to your account or make a shortlist. This allows you to compare and revisit your favorites as you make your decision."
+      />
     </>
   );
 };
