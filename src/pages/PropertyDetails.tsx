5fd7e8e77a86a8aca88562b212423fc5df96dc3d
YazanTawila <infoyazantawila@gmail.com>
2025-08-15 15:45:02 +0300

    Fix merge

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -3,13 +3,14 @@ import ContactForm from "../sections/Contact/ContactForm";
 import PricingDetailsSection from "../sections/propertiesDetails/PricingDetailsSection";
 import PropertyDetailsSlider from "../sections/propertiesDetails/PropertyDetailsSlider";
 import PropertyInfoSection from "../sections/FeatureAndDesc/PropertyInfoSection";
+import { ComprehensivePricingDetails } from "../data/PropertyDetails/ComprehensivePricingDetails";
 const PropertyDetails = () => {
   return (
     <div className="space-y-20 md:space-y-30 xl:space-y-37.5">
       <PropertyDetailsSlider />
       <PropertyInfoSection />
       <ContactForm title={detailsFormHeaderData.title} description={detailsFormHeaderData.description} data={detailsFormData.conactForm} areaText={detailsFormData.areaText} detailsPage />
-      <PricingDetailsSection/>
+      <PricingDetailsSection data={ComprehensivePricingDetails}/>
     </div>
   );
 };
