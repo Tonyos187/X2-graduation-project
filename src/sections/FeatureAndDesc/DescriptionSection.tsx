ae7f109676ddc38bd46174595d1a7d7a2504dd82
YazanTawila <infoyazantawila@gmail.com>
2025-08-17 14:35:35 +0300

    Edit | src/sections/FeatureAndDesc/ -edit style  DescriptionSection , FeaturesSection , PropetyInfoSection and edit style  FeaturesColumn

--- src/sections/FeatureAndDesc/DescriptionSection.tsx
+++ src/sections/FeatureAndDesc/DescriptionSection.tsx
@@ -12,87 +12,40 @@ export default function DescriptionSection({
   details,
 }: DescriptionSectionProps) {
   return (
-    <div className="rounded-[12px] border border-Grey-15 p-5 lg:p-10 xl:p-[50px] flex flex-col bg-Grey-08">
-      <h2 className="text-White text-lg lg:text-[20px] xl:text-2xl font-semibold">
-        {title}
-      </h2>
-
-      <p className="text-Grey-60 text-sm lg:text-[16px] xl:text-lg leading-relaxed pt-1.5 pb-5 lg:pt-2.5 lg:pb-[30px] xl:pt-3.5 xl:pb-10">
-        {description}
-      </p>
-
-      {/* للشاشات الكبيرة */}
-      <div className="hidden lg:flex gap-[20px] border-t-[1px] text-Grey-15 pt-5">
-        {/* القسم الأول */}
-        <div className="flex flex-col items-start gap-2 pr-3 lg:pr-5 border-r-[1px] text-Grey-15">
-          <div className="flex items-center gap-1 sm:gap-2">
-            <span className="text-Grey-60">{details[0]?.icon}</span>
-            <span className="text-Grey-60 text-sm xl:text-lg">{details[0]?.label}</span>
-          </div>
-          <span className="text-White text-lg lg:text-xl xl:text-2xl font-semibold">
-            {details[0]?.value}
-          </span>
-        </div>
-
-        {/* القسم الثاني */}
-        <div className="flex flex-col items-start gap-2 pr-3 xl:pr-5 border-r-[1px] text-Grey-15">
-          <div className="flex items-center gap-2">
-            <span className="text-Grey-60">{details[1]?.icon}</span>
-            <span className="text-Grey-60 text-sm xl:text-lg">{details[1]?.label}</span>
-          </div>
-          <span className="text-White text-lg lg:text-xl xl:text-2xl font-semibold">
-            {details[1]?.value}
-          </span>
-        </div>
-
-        {/* القسم الثالث */}
-        <div className="flex flex-col items-start gap-2">
-          <div className="flex items-center gap-2">
-            <span className="text-Grey-60">{details[2]?.icon}</span>
-            <span className="text-Grey-60 text-sm xl:text-lg">{details[2]?.label}</span>
-          </div>
-          <span className="text-White text-lg lg:text-xl xl:text-2xl font-semibold">
-            {details[2]?.value}
-          </span>
-        </div>
+    <div className=" lg:w-[49.0601%] w-full h-fit rounded-xl border border-Grey-15 p-5 
+      lg:p-10 xl:p-12.5 flex flex-col gap-5 md:gap-10 xl:gap-12.5 bg-Grey-08">
+      <div>
+        <h2 className="text-White text-lg md:text-xl xl:text-2xl font-semibold mb-1.5 md:mb-2.5 xl:mb-3.5">
+          {title}
+        </h2>
+        <p className="text-Grey-60 text-sm lg:text-base xl:text-lg leading-relaxed">
+          {description}
+        </p>
       </div>
-
-      {/* للشاشات الصغيرة */}
-      <div className="flex flex-col gap-3 border-t border-Grey-15 pt-3 lg:hidden">
-        {/* الصف الأول - Bed & Bath */}
-        <div className="flex  gap-2 sm:gap-5">
-          {/* Bed */}
-          <div className="flex flex-col items-start gap-2 pr-3 xl:pr-5 border-r border-Grey-15 flex-1">
-            <div className="flex items-center gap-2">
-              <span className="text-Grey-60">{details[0]?.icon}</span>
-              <span className="text-Grey-60 text-sm">{details[0]?.label}</span>
+      <div className="flex flex-wrap lg:flex-nowrap lg:justify-between gap-y-5">
+        {details.map((Card, index) => {
+          const isLast = index === details.length - 1;
+          return (
+            <div
+              key={index}
+              className={`
+                w-1/2 lg:w-auto flex flex-col gap-1.5 md:gap-2 xl:gap-2.5 
+                ${index !== 0 && !isLast ? "border-l border-Grey-15 pl-4" : ""}
+                ${isLast 
+                  ? "w-full border-t border-Grey-15 pt-5 lg:pt-0 lg:w-auto lg:border-t-0 lg:border-l lg:pl-4" 
+                  : ""}
+              `}
+              >
+              <div className="flex items-center gap-1 xl:gap-1.5">
+                <span className="text-Grey-60">{Card.icon}</span>
+                <span className="text-Grey-60 text-sm xl:text-lg font-medium">{Card.label}</span>
+              </div>
+              <span className="text-White text-lg lg:text-xl xl:text-2xl font-semibold">
+                {Card.value}
+              </span>
             </div>
-            <span className="text-White text-lg font-semibold">
-              {details[0]?.value}
-            </span>
-          </div>
-          {/* Bath */}
-          <div className="flex flex-col items-start gap-2 flex-1 pl-0 sm:pl-5">
-            <div className="flex items-center gap-2">
-              <span className="text-Grey-60">{details[1]?.icon}</span>
-              <span className="text-Grey-60 text-sm">{details[1]?.label}</span>
-            </div>
-            <span className="text-White text-lg font-semibold">
-              {details[1]?.value}
-            </span>
-          </div>
-        </div>
-
-        {/* الصف الثاني - Area */}
-        <div className="flex flex-col items-start gap-2 pt-3 border-t border-Grey-15">
-          <div className="flex items-center gap-2">
-            <span className="text-Grey-60">{details[2]?.icon}</span>
-            <span className="text-Grey-60 text-sm">{details[2]?.label}</span>
-          </div>
-          <span className="text-White text-lg font-semibold">
-            {details[2]?.value}
-          </span>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/sections/Images.tsx
+++ src/sections/Images.tsx
@@ -1,15 +1,13 @@
-import PropertyDetailsHeder from "../components/PropertyDetailsHeder";
-import Container from "../layouts/Container";
+import type { PropertyDetailsType } from "../types/PropertyDetails/PropertyDetailsTypes";
 import PropertyInfoSection from "./FeatureAndDesc/PropertyInfoSection";
 import PropertyDetailsSlider from "./propertiesDetails/PropertyDetailsSlider";
 
-function Images() {
+function Images({data}:{data:PropertyDetailsType}) {
     return (
-        <Container>
-            <PropertyDetailsHeder/>
-            <PropertyDetailsSlider/>
-            <PropertyInfoSection />
-        </Container>
+        <section className="pt-[128px] lg:pt-[157px] xl:pt-[220px] px-[4.1025%] md:px-[5.5555%] xl:px-[8.4375%] flex flex-col justify-between  gap-5 xl:gap-7.5 ">
+            <PropertyDetailsSlider data={data}/>
+            <PropertyInfoSection data={data}/>
+        </section>
     )
 }
 
