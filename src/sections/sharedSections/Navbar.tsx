de99ee5740a546695bd8d902541611d4e399d76c
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-08-19 01:17:59 +0300

    fix top banner

--- src/sections/sharedSections/Navbar.tsx
+++ src/sections/sharedSections/Navbar.tsx
@@ -66,9 +66,9 @@ const Navbar = (): JSX.Element => {
       {showBanner && (
         <div className="fixed top-0 left-0 w-full px-4 lg:px-8 bg-Grey-10 border-b border-Grey-15 z-[1100] h-[49px] xl:h-[63px] flex items-center overflow-hidden">
           <div className="relative w-full h-full text-center flex">
-            {/* الخلفية المدمرة يا الهييييييي */}
-            <div className="absolute inset-0 w-[200%] h-[1000%] -top-[100%] -left-[50%] object-cover">
-              <WavyBackdrop paths={HERO_PATH} />
+           
+            <div className="absolute inset-0 w-[150%] h-[750%] -top-[300%] -left-[50%] object-cover">
+              <WavyBackdrop  />
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-full xl:text-lg md:text-sm xs:text-[12px] pr-6.5 text-[10px]">
               <span className=" text-White xl:pr-2.5 pr-1.5">
