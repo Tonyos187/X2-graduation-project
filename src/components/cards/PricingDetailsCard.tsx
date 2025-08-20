ac60445facd5c784c7d054fd699c11c1d08aad32
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-08-20 22:16:32 +0300

    Edit-Feature|edit properties details button

--- src/components/cards/PricingDetailsCard.tsx
+++ src/components/cards/PricingDetailsCard.tsx
@@ -16,7 +16,7 @@ function PricingDetailsCard({PricingItem,title}:PricingDetailsCardPopps) {
         <div className="p-5 md:p-10 xl:p-12.5 border border-Grey-15 rounded-xl ">
             <div className="flex justify-between items-center border-b border-Grey-15 pb-5">
                 <h3 className="text-lg md:text-xl xl:text-2xl font-semibold urbanist">{title}</h3>
-                <Button content={"Learn More"} className={"border border-Grey-15"}/>
+                <Button content={"Learn More"} withBorder/>
             </div>
             <div className="flex flex-wrap ">
                 {PricingItem.map((data, index) => {
